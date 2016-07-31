17b42f8d59b8ee173c91bb0c1fe07969b9415ba3
Pengfei Xuan <pfxuan@gmail.com>
2016-07-31 01:26:03 -0400

    Fix run.sh for LinearRegression

--- LinearRegression/bin/run.sh
+++ LinearRegression/bin/run.sh
@@ -13,14 +13,14 @@ echo "========== running ${APP} workload =========="
 DU ${INPUT_HDFS} SIZE 
 
 JAR="${DIR}/target/LinearRegressionApp-1.0.jar"
-CLASS="LinearRegression.src.main.java.LinearRegressionApp"
+CLASS="LinearRegressionApp"
 OPTION=" ${INOUT_SCHEME}${INPUT_HDFS} ${INOUT_SCHEME}${OUTPUT_HDFS} ${MAX_ITERATION} "
 
-setup
+#setup
 for((i=0;i<${NUM_TRIALS};i++)); do
 	
-	RM  ${OUTPUT_HDFS}
-	purge_data "${MC_LIST}"	
+	#RM  ${OUTPUT_HDFS}
+	#purge_data "${MC_LIST}"
 	START_TS=`get_start_ts`
 	START_TIME=`timestamp`
 	echo_and_run sh -c "${SPARK_HOME}/bin/spark-submit --class $CLASS --master ${APP_MASTER} ${YARN_OPT} ${SPARK_OPT} ${SPARK_RUN_OPT} $JAR ${OPTION} 2>&1|tee ${BENCH_NUM}/LinearRegression_run_${START_TS}.dat"
@@ -29,7 +29,7 @@ for((i=0;i<${NUM_TRIALS};i++)); do
     get_config_fields >> ${BENCH_REPORT}
 	print_config  ${APP} ${START_TIME} ${END_TIME} ${SIZE} ${START_TS} ${res}>> ${BENCH_REPORT}
 done
-teardown
+#teardown
 
 exit 0
 
